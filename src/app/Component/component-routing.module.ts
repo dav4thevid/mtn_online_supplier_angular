7077c2e0d2d13f3169bb2883d863a8f469f26c8e
David <dav4thevid@gmail.com>
2020-12-25 16:16:01 +0100

    enhan:removed imports

--- src/app/Component/component-routing.module.ts
+++ src/app/Component/component-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ComponentsComponent } from './components/components.component';
-import { HomepageComponent } from './homepage/homepage.component';
 
 
 

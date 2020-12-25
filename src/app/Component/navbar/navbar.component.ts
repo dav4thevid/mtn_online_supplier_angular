51c3bb09e24c8405d36b332b98838e63f780ebe0
David <dav4thevid@gmail.com>
2020-12-25 16:02:24 +0100

    removed all commented codes to increase make performance

--- src/app/Component/navbar/navbar.component.ts
+++ src/app/Component/navbar/navbar.component.ts
@@ -3,18 +3,12 @@ import { ModalPopUpComponent } from './../modal-pop-up/modal-pop-up.component';
 // import { SearchModel } from "../models/searchModel";
 import {SearchModel} from '../models/searchModel';
 import { environment } from "../../../environments/environment.prod";
-import { Component, Input, OnInit } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 import { BehaviorSubject } from "rxjs";
-import { Router } from '@angular/router';
 
 
-
-
-//tenor
-var apikey = "WEFVEHVQMWTU";
-var lmt = 10;
 
 
 //giphy
@@ -42,23 +36,24 @@ export class NavbarComponent implements OnInit {
   httpOptions = {
     headers: new HttpHeaders({
       "Content-Type": "application/json",
-      // 'api_key': 'RSqt4yIW1nj82pXkiL5Fr6uGqItlWSlP'
       key: "WEFVEHVQMWTU",
     }),
   };
 
-  constructor(private http: HttpClient, private fb: FormBuilder, private router: Router, private modalService: NgbModal
+  constructor(
+      private http: HttpClient,
+      private fb: FormBuilder,
+      private modalService: NgbModal
 
      )
      {
 
-    // this.searchEndpointTenor = environment.baseUrlTenor + "search?q=";
+
     this.searchEndpointGiphy = environment.baseUrlGiphy + "gifs/search?";
 
   }
 
 
-  // https://api.giphy.com/v1/gifs/search?api_key=RSqt4yIW1nj82pXkiL5Fr6uGqItlWSlP&q=boys&limit=25&offset=0&rating=g&lang=en
 
   ngOnInit() {
     this.createSearch();
@@ -82,30 +77,10 @@ export class NavbarComponent implements OnInit {
 
   }
 
-  //Switch for Tenor API here
-  // async search(event) {
-  //   // console.log(event.target.value.toString());
-  //  await this.http
-  //     .get(
-  //       this.searchEndpointTenor +
-  //         event.target.value +
-  //         "&key=" +
-  //         apikey +
-  //         "&limit=" +
-  //         lmt
-  //     )
-  //     .subscribe((gifData: any) => {
-  //       this.data.next(gifData.results);
-  //       // console.log(gifData.results);
-  //     }
-  //     );
-  //     this.modalPopUp();
-  // }
 
 
-  async search(event) {
-    // console.log(event.target.value.toString());
-   await this.http
+  search(event) {
+   this.http
       .get(
         this.searchEndpointGiphy +
           "api_key=" +
@@ -122,7 +97,6 @@ export class NavbarComponent implements OnInit {
       )
       .subscribe((gifData: any) => {
         this.data.next(gifData.data);
-        // console.log(gifData.results);
       }
       );
       this.modalPopUp();

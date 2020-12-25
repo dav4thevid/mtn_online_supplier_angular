5c1165318a47d45206a6dc58e96851668f51f750
David <dav4thevid@gmail.com>
2020-12-25 16:13:55 +0100

    enhan:removed homepage routing

--- src/app/Component/component-routing.module.ts
+++ src/app/Component/component-routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ComponentsComponent } from './components/components.component';
 import { HomepageComponent } from './homepage/homepage.component';
-import { ListGifComponent } from './list-gif/list-gif.component';
 
 
 
@@ -12,23 +11,6 @@ const routes: Routes = [
     component: ComponentsComponent,
     children: [
       { path: '', redirectTo: 'homepage', pathMatch: 'full' },
-
-      {
-        path: 'homepage',
-        component: HomepageComponent,
-        data: { title: 'Homepage' },
-        pathMatch: 'full'
-     },
-
-      {
-        path: 'listgifs',
-        component: ListGifComponent,
-        data: { title: 'ListGifs',
-        pathMatch: 'full'
-      },
-
-     },
-
 
     ]
   }

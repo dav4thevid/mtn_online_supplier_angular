704b90fe2649f491393e54e28d4bdd334350f4e9
David <dav4thevid@gmail.com>
2021-01-06 13:03:24 +0100

    enhan:increased background image quality

--- src/app/Component/navbar/navbar.component.ts
+++ src/app/Component/navbar/navbar.component.ts
@@ -1,3 +1,4 @@
+// import { ImageserviceService } from './../../Services/imageservice.service';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { ModalPopUpComponent } from './../modal-pop-up/modal-pop-up.component';
 // import { SearchModel } from "../models/searchModel";
@@ -6,7 +7,8 @@ import { environment } from "../../../environments/environment.prod";
 import { Component, OnInit } from "@angular/core";
 import { HttpClient, HttpHeaders } from "@angular/common/http";
 import { FormBuilder, FormGroup, Validators } from "@angular/forms";
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, Observable } from "rxjs";
+import {ImageserviceService} from '../../Services/imageservice.service';
 
 //giphy
 var giphyKey = "RSqt4yIW1nj82pXkiL5Fr6uGqItlWSlP";
@@ -27,8 +29,11 @@ export class NavbarComponent implements OnInit {
   searchModel = new SearchModel();
   searchResults = {};
   data = new BehaviorSubject<any>([]);
+  datas = [];
   closeResult = '';
-  user = {name: 'David', profession: "coder"}
+  // user = {name: 'David', profession: "coder"}
+  isImageLoading;
+
 
   navbarCollapsed = true;
 
@@ -42,8 +47,8 @@ export class NavbarComponent implements OnInit {
   constructor(
       private http: HttpClient,
       private fb: FormBuilder,
-      private modalService: NgbModal
-
+      private modalService: NgbModal,
+      private imageService: ImageserviceService
      )
      {
     this.searchEndpointGiphy = environment.baseUrlGiphy + "gifs/search?";
@@ -58,6 +63,43 @@ export class NavbarComponent implements OnInit {
     modalRef.componentInstance.data = this.data;
   }
 
+  getImageFromService(event) {
+    this.isImageLoading = true;
+    this.imageService.getImage(
+      this.searchEndpointGiphy +
+          "api_key=" +
+          giphyKey +
+          "&q=" +
+          event.target.value +
+          "&limit =" +
+          limit +
+          "&offset=" +
+          offset +
+          "&rating=" +
+          rating +
+          "&lang=en",
+    ).subscribe(data => {
+      this.createImageFromBlob(data);
+      this.isImageLoading = false;
+      this.modalPopUp();
+    }, error => {
+      this.isImageLoading = false;
+      console.log(error);
+    });
+}
+
+imageToShow: any;
+
+createImageFromBlob(image: Blob) {
+  let reader = new FileReader();
+  reader.addEventListener("load", () => {
+     this.imageToShow = reader.result;
+  }, false);
+
+  if (image) {
+     reader.readAsDataURL(image);
+  }
+}
 
 
 toggleNavbarCollapse() {
@@ -77,7 +119,7 @@ toggleNavbarCollapse() {
   }
 
 
-  search(event) {
+  search(event){
    this.http
       .get(
         this.searchEndpointGiphy +
@@ -92,6 +134,7 @@ toggleNavbarCollapse() {
           "&rating=" +
           rating +
           "&lang=en",
+          { responseType: 'blob' }
       )
       .subscribe((gifData: any) => {
         this.data.next(gifData.data);
--- src/app/Services/imageservice.service.ts
+++ src/app/Services/imageservice.service.ts
@@ -1,9 +1,37 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+
+var giphyKey = "RSqt4yIW1nj82pXkiL5Fr6uGqItlWSlP";
+var limit = "15";
+var offset = "0";
+var rating = 'g';
 
 @Injectable({
   providedIn: 'root'
 })
-export class ImageserviceService {
 
-  constructor() { }
+
+
+export class ImageserviceService {
+  imageToShow: any;
+  data = new BehaviorSubject<any>([]);
+  searchEndpointGiphy: string;
+
+
+
+  constructor(
+    private http: HttpClient
+  ) {
+    this.searchEndpointGiphy = environment.baseUrlGiphy + "gifs/search?";
+   }
+
+
+   getImage(imageUrl: string): Observable<Blob> {
+    return this.http.get(imageUrl, { responseType: 'blob' });
+  }
+
+
+ 
 }
